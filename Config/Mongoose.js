c89e0591e563ef86b1a3f41ba4047ddf4add6a68
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-05-10 22:17:36 +0530

    feature-added mongoose and passport

---
+++ Config/Mongoose.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+
+// mongoose.connect(process.env["DB_URI"], {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost/quizez', {useNewUrlParser: true, useUnifiedTopology: true})
+
+const db=mongoose.connection;
+
+db.on('error', console.error.bind("Something went wrong with the Mongodb Server"));
+
+db.once("open",()=>console.log("Connection extablished with database"))
+
+module.exports=db;
---
+++ index.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const app=express();
+const cors=require('cors')
+const db=require('./Config/Mongoose')
+
+const router=require('./Routes');
+
+const PORT=8000;
+
+app.use(cors())
+app.use(express.json())
+
+const passportJWT=require('./Config/Passport')
+
+app.use("/api",router)
+
+app.use("/",(req,res)=>{
+    return res.status(404).json({
+        data:"Requested data doesn't exist"
+    })
+})
+
+app.listen(process.env.PORT ||PORT,()=>console.log("Server running on port "+PORT))
